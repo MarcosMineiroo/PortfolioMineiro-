03bb7005d5c9867be5cb35e149c185db34ef5315
MarcosMineiro <marcus.mineiro2016@gmail.com>
2025-01-24 16:14:59 -0300

    criação de opção de cor

--- assets/js/main.js
+++ assets/js/main.js
@@ -174,29 +174,30 @@ window.addEventListener('scroll', scrollUp)
 /*==================== DARK LIGHT THEME ====================*/ 
 const themeButton = document.getElementById('theme-button')
 const darkTheme = 'dark-theme'
-const iconTheme = 'bx-sun'
+const iconTheme = 'uil-sun'
 
-// Previously selected topic (if user selected)
+// Tópico selecionado anteriormente (se o usuário selecionou)
 const selectedTheme = localStorage.getItem('selected-theme')
 const selectedIcon = localStorage.getItem('selected-icon')
 
-// We obtain the current theme that the interface has by validating the dark-theme class
+// Obtem o tema atual da interface validando a classe do body
 const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
-const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx-moon' : 'bx-sun'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun'
 
-// We validate if the user previously chose a topic
+// Valida se o tema selecionado anteriormente existe
 if (selectedTheme) {
-  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+  // Se sim, ele adiciona o tema selecionado ao body
   document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
-  themeButton.classList[selectedIcon === 'bx-moon' ? 'add' : 'remove'](iconTheme)
+  // Se sim, ele adiciona o icone selecionado ao botão
+  themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
 }
 
-// Activate / deactivate the theme manually with the button
+// Altera o tema da interface ao clicar no botão
 themeButton.addEventListener('click', () => {
-    // Add or remove the dark / icon theme
+    // Adiciona ou remove o tema
     document.body.classList.toggle(darkTheme)
     themeButton.classList.toggle(iconTheme)
-    // We save the theme and the current icon that the user chose
+    // Salva o tema selecionado e o icone selecionado
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
