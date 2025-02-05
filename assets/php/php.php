af002b87870cb3510f4985df66c5d826a34642e4
MarcosMineiro <marcus.mineiro2016@gmail.com>
2025-02-05 19:27:40 -0300

    ajuste php

--- assets/php/php.php
+++ assets/php/php.php
@@ -2,12 +2,12 @@
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST["name"];
     $email = $_POST["email"];
-    $subject = $_POST["subject"];
+    $subject = $_POST["project"];
     $message = $_POST["message"];
 
     $to = "marcus.mineiro2016@gmail.com";
     $subject = "Nova mensagem de contato";
-    $body = "Nome: $name\nEmail: $email\nAssunto: $subject\nMensagem: $message";
+    $body = "Nome: $name\nEmail: $email\nProjeto: $project\nMensagem: $message";
     $headers = "From: $email";
 
     if (mail($to, $subject, $body, $headers)) {
